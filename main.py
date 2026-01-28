1f33dbf5290c634e865f0a6a808fd9dd5545add0
itz-nitant25 <nitant.amb252011@gmail.com>
2026-01-28 11:55:50 +0530

    main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI(title="Zeklo API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # later restrict to frontend URL
+    allow_origins=["*"],  # later restrict to frontend domain
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
