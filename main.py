e46504673da16ba41c4ad022802dabb29fda097f
itz-nitant25 <nitant.amb252011@gmail.com>
2026-01-28 09:15:03 +0530

    main.py

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ app.add_middleware(
 
 app.include_router(router)
 
-# Required for Render / production
 import uvicorn
-
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
