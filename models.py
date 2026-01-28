000547630e630d5f4d86c59878249ef3449bc09b
itz-nitant25 <nitant.amb252011@gmail.com>
2026-01-28 09:11:33 +0530

    models.py

--- models.py
+++ models.py
@@ -5,6 +5,7 @@ class Chat(Base):
     __tablename__ = "chats"
 
     id = Column(Integer, primary_key=True, index=True)
-    guest_id = Column(String, index=True)
+    guest_id = Column(String, index=True, nullable=True)
+    user_id = Column(Integer, nullable=True)
     message = Column(String)
     response = Column(String)
